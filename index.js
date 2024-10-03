54f82475e3c745e9edc0424c100de6111ffdaae3
mickey <mickey820519@vip.qq.com>
2024-10-03 14:02:43 +0800

    feat:  基础功能

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const DmmBaseConstant = require('./lib/constant/baseConstant');
+const DmmBaseError = require('./lib/error/baseError');
+const DmmBaseConfig = require('./lib/config/baseConfig');
+const DmmBaseModel = require('./lib/model/baseModel');
+const DmmBaseController = require('./lib/controller/baseController');
+const DmmBaseApi = require('./lib/api/baseApi');
+
+module.exports = {
+  DmmBaseConstant,
+  DmmBaseError,
+  DmmBaseConfig,
+  DmmBaseModel,
+  DmmBaseController,
+  DmmBaseApi
+};
